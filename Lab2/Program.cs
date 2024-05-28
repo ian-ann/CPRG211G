4b9c43fca10568c7488c3900bc79320f1c4f36d6
marian.estrada <ian_an@yahoo.com>
2024-05-28 08:45:42 -0600

    Squashed commit of the following:
    commit 8c0b7ff3005290e94c63492ae91914f88fbe5816
    Author: marian.estrada <ian_an@yahoo.com>
    Date:   Tue May 28 07:14:40 2024 -0600
    
        Squashed commit of the following:
    
        commit b315fa45b910f91dc18df7aeca95503f8691d216
        Author: marian.estrada <ian_an@yahoo.com>
        Date:   Tue May 28 06:31:02 2024 -0600
    
            Lab1 Code

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -6,6 +6,57 @@ using System.Threading.Tasks;
 
 namespace Lab2
 {
+    public class Employee
+    {
+        private string ID { get; set; }
+        private string Name { get; set; }
+        private string Address { get; set; }
+        private string Phone { get; set; }
+        private long Sin { get; set; }
+        private string DoB { get; set; }
+        private string Dept { get; set; }
+
+
+
+        public void Employee();
+    }
+
+    public class Salaried : Employee
+    {
+        public double Salary { get; set; }
+
+        public override double CalculateWeeklyPay()
+        {
+            return Salary;
+        }
+    }
+
+    public class PartTime : Employee
+    {
+        public double HourlyRate { get; set; }
+        public double HoursWorked { get; set; }
+
+        public override double CalculateWeeklyPay()
+        {
+            return HourlyRate * HoursWorked;
+        }
+    }
+
+    public class Wages : Employee
+    {
+        public double HourlyRate { get; set; }
+        public double HoursWorked { get; set; }
+
+        public override double CalculateWeeklyPay()
+        {
+            if (HoursWorked <= 40)
+                return HourlyRate * HoursWorked;
+            else
+                return (HourlyRate * 40) + ((HoursWorked - 40) * HourlyRate * 1.5);
+        }
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
