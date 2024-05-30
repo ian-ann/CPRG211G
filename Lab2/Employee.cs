b2792db3eda2a7170255ceecfe906722c3fe7227
marian.estrada <ian_an@yahoo.com>
2024-05-30 14:31:39 -0600

    Added Lab2 in development branch (no merge)

---
+++ Lab2/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Employee
+    {
+        private string ID { get; set; }
+        private string Name { get; set; }
+        private string Address { get; set; }
+        private string Phone { get; set; }
+        private long Sin { get; set; }
+        private string DoB { get; set; }
+        private string Dept { get; set; }
+
+        public Employee() { 
+            }
+
+        public Employee(string id, string name, string address, string phone, long sin, string dob, string dept)
+        {
+            ID = id;
+            Name = name;
+            Address = address;
+            Phone = phone;
+            Sin = sin;
+            DoB = dob;
+            Dept = dept;
+        }
+
+        public override string ToString() 
+        { 
+            return Name;
+        }
+
+    }
+}
---
+++ Lab2/Wages.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Wages : Employee
+    {
+        private double Rate { get; set; }
+        private double Hours { get; set; }
+        private string name { get; set; }
+
+        public Wages() { }
+        public Wages(string id, string name, string address, string phone, long sin, string dateOfBirth,
+		string dept, double rate, double hours) : base(id, name, address, phone, sin, dateOfBirth, dept)
+            { 
+                this.Rate = rate;
+                this.Hours = hours;
+                this.name = name;
+            }
+
+
+        public double GetPay()
+            {
+            	/*Wage employees’ pay is calculated using hourly rate * work hours with overtime paid at time and a half for any hours worked over 40 in one week.*/
+                double pay = 0;
+                if (this.Hours <= 40)
+                    pay = this.Hours  * this.Rate;
+                else
+                    pay =  (this.Rate * 40) + ((this.Hours - 40) * this.Rate * 1.5);
+
+                return pay; 
+            }
+
+        public void ToString(){}
+    }
+
+}
