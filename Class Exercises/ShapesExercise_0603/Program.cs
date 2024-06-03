4c240ad71dc95ab2308686929660464f8e81b0a3
marian.estrada <ian_an@yahoo.com>
2024-06-03 13:05:19 -0600

    Update code based on June 3 Exercises

---
+++ Class Exercises/ShapesExercise_0603/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractExample
+{
+    abstract class Shape
+    {
+        public abstract double area();
+            }
+
+    class Circle : Shape 
+    {
+        private double radius; 
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+        public override double area()
+        {
+            return (3.14*radius*radius);
+        }
+    }
+
+    class Square: Shape
+    {
+        private double side;
+        public Square(double s)
+        {
+            side = s;
+        }
+
+        public override double area()
+        {
+            return (side*side);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(5.0);
+            Console.WriteLine("Area of Circle = {0}", c.area());
+            Square s = new Square(2.5);
+            Console.WriteLine("Area of Square = {0}",s.area());
+        }
+    }
+}
---
+++ Class Exercises/SpaceStation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceStation
+{
+
+    abstract class SpaceSation
+    {
+        public abstract void FireLaser();
+    }
+
+    class DeathStar : SpaceSation
+    {
+
+        public override void FireLaser() {
+            Console.WriteLine("Phew phew");
+            }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeathStar c = new DeathStar();
+            c.FireLaser();
+        }
+    }
+}
